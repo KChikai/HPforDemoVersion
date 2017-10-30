3bba5001cc8286827cd18dca0cedb7ab118de54d
KChikai <tokoroten0401@gmail.com>
2017-10-30 20:33:20 +0900

    fix some parts

--- tree_view/admin.py
+++ tree_view/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from tree_view.models import New
+
 
 # Register your models here.
+
+class NewAdmin(admin.ModelAdmin):
+    list_display = ('text_ja', 'text_en', 'date', 'image')
+
+admin.site.register(New, NewAdmin)
--- tree_view/models.py
+++ tree_view/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class New(models.Model):
+    date = models.DateField()
+    text_ja = models.TextField(max_length=200, blank=True)
+    text_en = models.TextField(max_length=200, blank=True)
+    image = models.ImageField(upload_to="./images", blank=True)
