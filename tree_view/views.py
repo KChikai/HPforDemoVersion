144380e101f8a4532451cb58267b25650a502202
nearstructure <tokoroten0401@gmail.com>
2017-10-31 05:15:58 +0900

    fix design

--- tree_view/views.py
+++ tree_view/views.py
@@ -10,8 +10,6 @@ def index(request):
 def embedding(request):
     # 最新5件のニュースをデータベースから取得
     obj_latest_5 = New.objects.order_by('-date')[:5]
-    for news in obj_latest_5:
-        print(news.date, type(news.date))
     context = {'latest_news_list': obj_latest_5}
     if len(context['latest_news_list']) == 0:
         return render(request, APP_LABEL + '/test_embedding.html')
